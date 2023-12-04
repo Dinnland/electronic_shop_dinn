2b7a767aabd4a284c2bbc5e8de0cd2fffb866d0a
Dinar Mingazov <dinarek2013@gmail.com>
2023-12-04 14:42:38 +0300

    Добавлена ссылка на поставщика оборудования в админ панели

--- supplier/admin.py
+++ supplier/admin.py
@@ -5,17 +5,12 @@ from supplier.models import Supplier, Product
 from django.utils.html import format_html
 
 
-# @admin.register(Supplier)
-
-
+@admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'previous_supplier', 'debt_amount', 'supplier_type', 'ranking')
-    # list_display = ('name','supplier_link', 'previous_supplier', 'debt_amount', 'supplier_type', 'ranking')
-    readonly_fields = ['ranking']
-    # readonly_fields = ['ranking', 'supplier_link']
+    list_display = ('name', 'supplier_link', 'debt_amount', 'supplier_type', 'ranking')
+    readonly_fields = ['ranking', 'supplier_link']
     search_fields = ("name",)
     list_filter = ["city", "country"]
-    # inlines = ['previous_supplier',]
     actions = ("clear_the_debt",)  # Necessary
 
     @admin.action(description='Очистить задолженность')
@@ -38,40 +33,15 @@ class SupplierAdmin(admin.ModelAdmin):
             instance.save()
         formset.save()
 
-
-#     def user_link(self, obj):
-#         return mark_safe('<a href="{}">{}</a>'.format(
-#             reverse("admin:auth_user_change", args=(obj.user.pk,)),
-#             obj.user.email
-#         ))
-#
-#     user_link.short_description = 'user'
-#
-# admin.site.register(Supplier, SupplierAdmin)
-#     def supplier_link(self, obj):
-#         # Generate a link to the related Book admin page for the author
-#         # print(obj.previous_supplier)
-#         # print(type(obj.previous_supplier))
-#         j = obj.previous_supplier
-#         if j != 'None':
-#             print('j',j)
-#         # print('get(name=j)', Supplier.objects.get(name=j))
-#         print('filter(name=j)', Supplier.objects.filter(name=j))
-#         if Supplier.objects.filter(name=j).exists():
-#             print(Supplier.objects.filter(name=j))
-#         # if obj.previous_supplier != 'None':
-#             # print(obj)
-#         object_id = 17
-#         # link = reverse('admin:supplier_supplier_changelist') + f'?supplier_id={obj.previous_supplier}'
-#         # return format_html(f'<a href="{object_id}">{obj.previous_supplier}</a>', link, object_id=17)
-#         #
-#         link = reverse('admin:supplier_supplier_changelist') + f'?supplier_id={17}'
-#         return format_html(f'<a href="{object_id}">{obj.previous_supplier}</a>', link)
-#
-#     supplier_link.short_description = 'Поставщик оборудования'  # Column header
-
-
-admin.site.register(Supplier, SupplierAdmin)
+    def supplier_link(self, obj):
+        """Получение ссылки на поставщика"""
+        prev_supp = obj.previous_supplier
+        if prev_supp is not None:
+            filter_supplier = Supplier.objects.filter(name=prev_supp.name)
+            prev_supp_pk = filter_supplier.first().pk
+            link = reverse('admin:supplier_supplier_changelist')
+            return format_html(f'<a href="{prev_supp_pk}">{obj.previous_supplier}</a>', link)
+    supplier_link.short_description = 'Поставщик оборудования'  # Column header
 
 
 @admin.register(Product)
